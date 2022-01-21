d9fd62874bd6e3e1f201e306d3cc47b08b0ceb7e
Kristjan Kõiv <kristjankv32@gmail.com>
2022-01-21 17:52:12 +0200

    #22 Start of implementation.

--- app/models.py
+++ app/models.py
@@ -1,8 +1,77 @@
 from django.db import models
 
-class Posts(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField()
 
-    def __str__(self):
-        return self.title
+class User(models.Model):
+    uni_id = models.CharField(max_length=50)
+    password_hash = models.CharField(max_length=100)
+
+
+class ProjectGroup(models.Model):
+    project_group = models.ForeignKey('self', on_delete=models.SET_NULL)
+
+
+class Project(models.Model):
+    project_group = models.ForeignKey(ProjectGroup, on_delete=models.SET_NULL)
+
+
+class Repository(models.Model):
+    url = models.CharField(max_length=100)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+
+class UserProject(models.Model):
+    class Rights(models.TextChoices):
+        ADMIN = "A", _("Admin")
+        OWNER = "O", _("Owner")
+        VIEWER = "V", _("Viewer")
+
+    rights = models.CharField(max_length=1, choices=Rights.choices, default=Rights.VIEWER)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+
+class GradeCategory(models.Model):
+    pass
+
+
+class GradeMilestone(models.Model):
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    grade_category = models.ForeignKey(GradeCategory, on_delete=)
+
+
+class Milestone(models.Model):
+    grade_milestone = models.ForeignKey(GradeMilestone, on_delete=models.SET_NULL)
+    repository = models.ForeignKey(Repository, on_delete=models.CASCADE)
+
+
+class Issue(models.Model):
+    pass
+
+
+class IssueMilestone(models.Model):
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
+    milestone = models.ForeignKey(Milestone, on_delete=models.CASCADE)
+
+
+class TimeSpent(models.Model):
+    amount = models.IntegerField()
+    time = models.DateTimeField()
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Commit(models.Model):
+    hash = models.CharField(max_length=100)
+    time = models.DateTimeField()
+    message = models.TextField()
+    lines_added = models.IntegerField()
+    lines_removed = models.IntegerField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    repository = models.ForeignKey(Repository, on_delete=models.CASCADE)
+
+
+class GradeCalculation(models.Model):
+    project_group = models.ForeignKey(ProjectGroup, on_delete=models.CASCADE)
