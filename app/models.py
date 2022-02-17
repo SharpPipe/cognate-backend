55ecf821897dbaf1eecbd41bf66a44dbfba2aeb5
Kristjan Kõiv <kristjankv32@gmail.com>
2022-02-17 04:34:28 +0200

    #39 ProjectGroups are displayed with their name

--- app/models.py
+++ app/models.py
@@ -26,6 +26,9 @@ class ProjectGroup(models.Model):
     group_id = models.IntegerField(null=True, blank=True)
     parent_group = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Project(models.Model):
     name = models.CharField(max_length=255, null=True, blank=True)
